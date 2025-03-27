ac3184c735f48c838e4dbde5c238d9cfef657004
AthaSSiN <athassin.asp@gmail.com>
2025-03-27 15:47:37 -0400

    updates

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install htop dstat python3-pip
+sudo apt-get install htop dstat python3-pip -y
 echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
     
