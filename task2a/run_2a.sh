12585f43ca3650d3ef7334a141da96be4c9f3cf5
AthaSSiN <athassin.asp@gmail.com>
2025-03-27 21:20:37 -0400

    updates

--- task2a/run_2a.sh
+++ task2a/run_2a.sh
@@ -4,7 +4,7 @@ export GLUE_DIR=/proj/cos568proj2-PG0/glue_data
 export TASK_NAME=RTE
 export PATH=$PATH:/users/atharvsp/.local/bin
 rm task2a/run_2a_$1.out
-python3 temp.py \
+python3 task2a/run_glue.py \
   --model_type bert \
   --model_name_or_path bert-base-cased \
   --task_name $TASK_NAME \
@@ -12,7 +12,7 @@ python3 temp.py \
   --do_eval \
   --data_dir $GLUE_DIR/$TASK_NAME \
   --max_seq_length 128 \
-  --per_device_train_batch_size 64 \
+  --per_device_train_batch_size 8 \
   --learning_rate 2e-5 \
   --num_train_epochs 1 \
   --output_dir /tmp/$TASK_NAME/ \
