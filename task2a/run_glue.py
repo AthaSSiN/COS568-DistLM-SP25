f299c0b04fe6c253e28b5e29594222ab75b9f583
AthaSSiN <athassin.asp@gmail.com>
2025-03-27 20:13:20 -0400

    updates

--- task2a/run_glue.py
+++ task2a/run_glue.py
@@ -115,7 +115,7 @@ def train(args, train_dataset, model, tokenizer):
     set_seed(args)  # Added here for reproductibility (even between python 2 and 3)
     for _ in train_iterator:
         epoch_iterator = tqdm(train_dataloader, desc="Iteration", disable=args.local_rank not in [-1, 0])
-        for step, batch in enumerate(epoch_iterator[0:1]):
+        for step, batch in enumerate(epoch_iterator):
             start_time = time.perf_counter()
             model.train()
             batch = tuple(t.to(args.device) for t in batch)
